2618f97d6383cc593d5754de6e86909152967499
Ahmad Kayyali <kayyali18@gmail.com>
2021-01-18 19:16:17 -0500

    Create TodoItem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type Props = TodoProps & {
+  updateTodo: (todo: ITodo) => void;
+  deleteTodo: (_id: String) => void;
+};
+
+const Todo: React.FC<Props> = ({ todo, updateTodo, deleteTodo }) => {
+  const checkTodo: string = todo.status ? `line-through` : "";
+  const buttonStatus: string = todo.status
+    ? `hide-button`
+    : "Card--button__done";
+
+  return (
+    <div className="Card">
+      {/* Text in the card */}
+      <div className="Card--text">
+        <h1 className={checkTodo}>{todo.name}</h1>
+        <span className={checkTodo}>{todo.description}</span>
+      </div>
+
+      {/* Buttons for manipulating card */}
+      <div className="Card--button">
+        <button className={buttonStatus} onClick={() => updateTodo(todo)}>
+          Complete
+        </button>
+        <button
+          className="Card--button__delete"
+          onClick={() => {
+            deleteTodo(todo._id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
