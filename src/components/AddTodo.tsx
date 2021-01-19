bf3066318ca075178d32af242754f1ed54a08c2f
Ahmad Kayyali <kayyali18@gmail.com>
2021-01-18 20:47:31 -0500

    Add input handling to Add Todo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 type Props = {
   saveTodo: (e: React.FormEvent, formData: ITodo | any) => void;
 };
 
 const AddTodo: React.FC<Props> = ({ saveTodo }) => {
+  const [nameValue, setNameValue] = useState("");
+  const [descValue, setDescValue] = useState("");
+
+  useEffect(() => {}, [descValue, nameValue]);
+
+  // Form submit handler
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const form = e.target as any;
+    const formData = Object.fromEntries(new FormData(form));
+    saveTodo(e, formData);
+  };
+
+  // Form input hanlder
+  const handleInput = (e: React.FormEvent) => {
+    const target = e.target as any;
+    // console.log(target);
+    target.name === "name"
+      ? setNameValue(target.value)
+      : setDescValue(target.value);
+  };
+
+  const isDisabled = (): boolean => {
+    return nameValue && descValue ? false : true;
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        const form = e.target as any;
-        const formData = new FormData(form);
-        let obj = Object.fromEntries(new FormData(form));
-        console.log(obj);
-      }}
-      className="Form"
-    >
+    <form onSubmit={handleSubmit} className="Form">
       <h1>My Todos</h1>
       <div>
-        <input type="text" name="name" />
-        <textarea name="description" cols={30} rows={10}></textarea>
-        <button type="submit">Save Todo</button>
+        <input onInput={handleInput} type="text" name="name" />
+        <textarea
+          onInput={handleInput}
+          name="description"
+          cols={30}
+          rows={10}
+        ></textarea>
+        <button type="submit" disabled={isDisabled() ? true : false}>
+          Save Todo
+        </button>
       </div>
     </form>
   );
